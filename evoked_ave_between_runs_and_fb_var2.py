b30b098ecefa91a9d8fd40aedc8c50e1b7e694b4
Vera Tretyakova <vera.d.tretyakova@gmail.com>
2021-06-03 13:01:26 +0300

    Add files via upload

--- evoked_ave_between_runs_and_fb_var2.py
+++ evoked_ave_between_runs_and_fb_var2.py
@@ -21,6 +21,7 @@ subjects.remove('P036')
 subjects.remove('P049')
 subjects.remove('P056')
 
+'''
 ###################### при построении topomaps берем только тех испытуемых, у которых есть все категории условий ####################
 ### extract subjects with all conditions:fb+trial_type ####
 cond_list = ['_norisk_fb_cur_positive',
@@ -48,6 +49,7 @@ for i,subject in enumerate(subjects):
 subjects = subj_list
 
 print(len(subjects))
+'''
 
 rounds = [1, 2, 3, 4, 5, 6]
 
@@ -63,42 +65,74 @@ for t in trial_type:
     for subj in subjects:
         evoked_internal_subjects = []
         for r in rounds:
+            #print(len(evoked_internal_subjects)) 
             try:
-                #print(len(evoked_internal_subjects))    
+                       
                 epochs_positive = mne.read_epochs('/net/server/data/Archive/prob_learn/vtretyakova/Nikita_mio_cleaned/beta_16_30_epo/{0}_run{1}_{2}_fb_cur_positive_beta_16_30-epo.fif'.format(subj, r, t), preload = True)
-                epochs_negative = mne.read_epochs('/net/server/data/Archive/prob_learn/vtretyakova/Nikita_mio_cleaned/beta_16_30_epo/{0}_run{1}_{2}_fb_cur_negative_beta_16_30-epo.fif'.format(subj, r, t), preload = True)
-
+                    
+                p = True
+            except (OSError):
+                p = False
+                print('This file not exist')
+                    
+            try:   
+                epochs_negative = mne.read_epochs('/net/server/data/Archive/prob_learn/vtretyakova/Nikita_mio_cleaned/beta_16_30_epo/{0}_run{1}_{1}_fb_cur_negative_beta_16_30-epo.fif'.format(subj, r, t), preload = True)
+                    
+                n = True
+                    
+            except (OSError):
+                n = False
+                print('This file not exist')    
+                
+            # если оба фид бека есть у испытуемого, то объединяем их
+            if p is True and n is True:
                 #эпохи соединяются как np.array и без извлечения данных (get_data())
                 epochs = np.vstack([epochs_positive, epochs_negative])
-                
-                ################ Шаг усреднения 1 - усреднение внутри блока #################
-                
-                evoked = epochs.mean(axis=0) # shape 306 x  n, n - кол - во временных отчетов 
+                # получаем усредненные данные для одного блока
+                evoked = epochs.mean(axis=0) # shape 306 x  n, n - кол - во временных отчетов
                 # получаем список evoked усредненных внутри блока (длинна списка от 0 до 6, по количеству блоков)
                 evoked_internal_subjects.append(evoked)
-            except (OSError):
-                print('This file not exist')
-            
-            
-            if len(evoked_internal_subjects) > 0:
-                #берем первый evoked, добавляем еще одну ось для блока
-                evoked_array_for_subj = evoked_internal_subjects[0].reshape(1, 306, 1350)
-                # так же добавляем еще одну ось для каждого блока и соединяем их в единый массив
-                for i in np.arange(1, len(evoked_internal_subjects)):
-                    a = evoked_internal_subjects[i].reshape(1, 306, 1350)
-                    evoked_array_for_subj = np.vstack([evoked_array_for_subj, a])
-                    
-                    
-                ############## Шаг усреднения 2 - усредняем данные внутри испытуемого #########################
-                evoked_for_subject = evoked_array_for_subj.mean(axis = 0)
                 
-                # делаем Evoked с помощью донора (поскольку Evoked берем из тех же данных, то время не меняем)
-                temp.data = evoked_for_subject
-                # сохраняем данные, усредненные внутри испытуемого. Шаг усредения 3, это усреднение между испытуемыми делается при рисовании топомапов
-                temp.save('/net/server/data/Archive/prob_learn/vtretyakova/Nikita_mio_cleaned/beta_16_30_ave_into_subjects_2_steps/{0}_{1}_evoked_beta_16_30_resp.fif'.format(subj, t))
+            # если только положительные, то в эвокд идет только положительный фид бек
+            elif p is True and n is False:
+                epochs = epochs_positive
+                evoked = epochs.get_data().mean(axis=0)
+                evoked_internal_subjects.append(evoked)
                 
+                # если только отрицательный, то в эвокд идет только отрицательный фид бек
+            elif p is False and n is True:
+                epochs = epochs_negative
+                evoked = epochs.get_data().mean(axis=0)
+                evoked_internal_subjects.append(evoked)
+                    
+                # если в данном блоке вообще нет трайлов, то пропускаем его    
             else:
                 pass
+                #except (OSError):
+                #    print('This file not exist')
+                        
+        print('evoked %s'%len(evoked_internal_subjects))
+                    
+                    
+        if len(evoked_internal_subjects) > 0:
+        #берем первый evoked, добавляем еще одну ось для блока
+            evoked_array_for_subj = evoked_internal_subjects[0].reshape(1, 306, 1350)
+            # так же добавляем еще одну ось для каждого блока и соединяем их в единый массив
+            for i in np.arange(1, len(evoked_internal_subjects)):
+                a = evoked_internal_subjects[i].reshape(1, 306, 1350)
+                evoked_array_for_subj = np.vstack([evoked_array_for_subj, a])
+                        
+                        
+            ############## Шаг усреднения 2 - усредняем данные внутри испытуемого #########################
+            evoked_for_subject = evoked_array_for_subj.mean(axis = 0)
+                    
+            # делаем Evoked с помощью донора (поскольку Evoked берем из тех же данных, то время не меняем)
+            temp.data = evoked_for_subject
+            # сохраняем данные, усредненные внутри испытуемого. Шаг усредения 3, это усреднение между испытуемыми делается при рисовании топомапов
+            temp.save('/net/server/data/Archive/prob_learn/vtretyakova/Nikita_mio_cleaned/beta_16_30_ave_into_subjects_2_steps/{0}_{1}_evoked_beta_16_30_resp.fif'.format(subj, t))
+                    
+        else:
+            pass
             
            
                     
