b129cddd27e7791baf269fba9b37c05201137c87
Vera Tretyakova <vera.d.tretyakova@gmail.com>
2021-10-15 16:09:19 +0300

    Add files via upload

---
+++ get_freq_stc.py
@@ -0,0 +1,74 @@
+import mne
+import os
+import os.path as op
+import numpy as np
+import pandas as pd
+from functions import make_freq_stc
+
+
+L_freq = 16
+H_freq = 30
+f_step = 2
+
+#time_bandwidth = 4 #(by default = 4)
+# if delta (1 - 4 Hz) 
+#n_cycles = np.array([1, 1, 1, 2]) # уточнить
+
+
+period_start = -1.750
+period_end = 2.750
+
+baseline = (-0.35, -0.05)
+
+freq_range = 'beta_16_30'
+
+description = 'STC for risk negative and positive FB'
+
+# This code sets an environment variable called SUBJECTS_DIR
+os.environ['SUBJECTS_DIR'] = '/net/server/data/Archive/prob_learn/freesurfer'
+subjects_dir = '/net/server/data/Archive/prob_learn/freesurfer'
+
+# 40 subjects with all choice types
+subjects = pd.read_csv('/home/vtretyakova/Рабочий стол/probability_learning/sources/subj_list.csv')['subj_list'].tolist()
+
+subjects.remove('P062') #without MRI
+subjects.remove('P052') # bad segmentation 
+
+
+rounds = [1, 2, 3, 4, 5, 6]
+
+trial_type = ['risk'] # on this step we'll make stc only for risk positive and negative fb
+#trial_type = ['norisk', 'prerisk', 'risk', 'postrisk']
+
+feedback = ['positive', 'negative']
+
+data_path = '/net/server/data/Archive/prob_learn/vtretyakova/ICA_cleaned'
+os.makedirs('/net/server/data/Archive/prob_learn/vtretyakova/sources/{0}'.format(freq_range), exist_ok = True)
+os.makedirs('/net/server/data/Archive/prob_learn/vtretyakova/sources/{0}/{0}_stc_fsaverage'.format(freq_range), exist_ok = True)
+
+########################## Обязательно делать файл, в котором будет показано какие параметры были заданы, иначе проверить вводные никак нельзя, а это необходимо при возникновении некоторых вопросов ############################################
+
+lines = ["freq_range = {}".format(freq_range), description, "L_freq = {}".format(L_freq), "H_freq = {}, в питоне последнее число не учитывается, т.е. по факту частота (H_freq -1) ".format(H_freq), "f_step = {}".format(f_step), "period_start = {}".format(period_start), "period_end = {}".format(period_end), "baseline = {}".format(baseline)]
+
+
+with open("/net/server/data/Archive/prob_learn/vtretyakova/sources/{0}/{0}_stc_fsaverage/config.txt".format(freq_range), "w") as file:
+    for  line in lines:
+        file.write(line + '\n')
+
+
+##############################################################################################################
+
+
+for subj in subjects:
+    bem = mne.read_bem_solution('/net/server/data/Archive/prob_learn/vtretyakova/sources/bem/{0}_bem.h5'.format(subj), verbose=None)
+    src = mne.setup_source_space(subject =subj, add_dist=False )
+    for r in rounds:
+        for cond in trial_type:
+            for fb in feedback:
+                try:
+                    
+                    stc_fsaverage = make_freq_stc(subj, r, cond, fb, data_path, L_freq, H_freq, f_step, period_start, period_end, baseline, bem, src)
+                    stc_fsaverage.save('/net/server/data/Archive/prob_learn/vtretyakova/sources/{0}/{0}_stc/{1}_run{2}_{3}_fb_cur_{4}_{0}_stc'.format(freq_range, subj, r, cond, fb))
+                except (OSError):
+                    print('This file not exist')
+
